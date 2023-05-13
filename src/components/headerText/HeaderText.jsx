1c5f5662b3525d78029d781e2d44e6355d25acd7
Elnura Dzhu <eln2806@gmail.com>
2023-05-12 21:02:29 -0500

    random number generated

--- src/components/headerText/HeaderText.jsx
+++ src/components/headerText/HeaderText.jsx
@@ -1,9 +1,34 @@
 import "./headreText.style.css";
 
 const HeaderText = () => {
+  const getRandomNum = () => {
+    const myNum = Math.floor(Math.random() * 10 + 1);
+    let message;
+    if (myNum % 2 == 0) {
+      message = `${myNum} is Even`;
+    } else {
+      message = `${myNum} is Odd`;
+    }
+    return message;
+  };
+  // const students = ["Murat", "Shahzod", "Tom", "Alina", "Esma"];
   return (
     <>
-      <h1>I am a header Text</h1>;<h2>I am the second header Text</h2>;
+      <h1>{getRandomNum()}</h1>
+      {/* <h1>Students lists</h1> */}
+      {/* <ol>
+        <li>{students[0]}</li>
+        <li>{students[1]}</li>
+        <li>{students[2]}</li>
+        <li>{students[3]}</li>
+        <li>{students[4]}</li>
+      </ol> */}
+
+      {/* <ol>
+        {students.map((el) => (
+          <li>{el}</li>
+        ))}
+      </ol> */}
     </>
   );
 };
